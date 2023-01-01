b10a62f6952f00219d1ee7a162bd2953e6d5b320
abassGarane <thelibertinewreck@gmail.com>
2023-01-01 14:38:45 +0300

    added cors

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -4,6 +4,7 @@ import (
 	"github.com/abassGarane/muscles/domain"
 	"github.com/go-chi/chi/middleware"
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
 )
 
 func initRouter(s domain.Service) chi.Router {
@@ -14,6 +15,18 @@ func initRouter(s domain.Service) chi.Router {
 	router.Use(middleware.Heartbeat("/ping"))
 	router.Use(middleware.AllowContentType("application/json"))
 	router.Use(middleware.SetHeader("Content-Type", "application/json"))
+
+	//cors config
+	router.Use(cors.Handler(cors.Options{
+		// AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
+		AllowedOrigins: []string{"https://*", "http://*"},
+		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	}))
 
 	// personal middleware
 
--- go.mod
+++ go.mod
@@ -10,6 +10,7 @@ require (
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/go-chi/cors v1.2.1 // indirect
 	github.com/go-playground/locales v0.14.0 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
 	github.com/go-playground/validator/v10 v10.11.1 // indirect
