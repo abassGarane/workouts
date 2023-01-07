522cee5c1a92aced097b05d2f5c9466fadc9026e
abassGarane <thelibertinewreck@gmail.com>
2023-01-07 08:47:12 +0300

    interface methods to manage users

--- domain/repository.go
+++ domain/repository.go
@@ -1,9 +1,17 @@
 package domain
 
+import "github.com/abassGarane/muscles/domain/models"
+
 type Repository interface {
+	// Workouts
 	AddWorkout(*Workout) (*Workout, error)
 	GetWorkout(string) (*Workout, error)
 	GetWorkouts() ([]*Workout, error)
-	DeleteWorkout(string)error
-	UpdateWorkout(string, *Workout)(*Workout, error)
+	DeleteWorkout(string) error
+	UpdateWorkout(string, *Workout) (*Workout, error)
+
+	//Users
+	CreateUser(*models.User) (*models.User, error)
+	UpdateUser(string, *models.User) (*models.User, error)
+	DeleteUser(string) error
 }
