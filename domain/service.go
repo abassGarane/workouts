ee853696866f649c7152ee39ae93ab4da41e0e54
abassGarane <thelibertinewreck@gmail.com>
2023-01-08 12:16:33 +0300

    user services defind in the logic impl

--- domain/service.go
+++ domain/service.go
@@ -1,9 +1,17 @@
 package domain
 
+import "github.com/abassGarane/muscles/domain/models"
+
 type Service interface {
+	//workout services
 	CreateWorkout(*Workout) (*Workout, error)
 	GetWorkout(string) (*Workout, error)
 	GetWorkouts() ([]*Workout, error)
-	DeleteWorkout(string)error
-	UpdateWorkout(string, *Workout)(*Workout, error)
+	DeleteWorkout(string) error
+	UpdateWorkout(string, *Workout) (*Workout, error)
+
+	//user services
+	GetUserByEmail(string) (*models.User, error)
+	CreateUser(*models.UserRequest) (*models.User, error)
+	UpdateUser(string, *models.User) (*models.User, error)
 }
