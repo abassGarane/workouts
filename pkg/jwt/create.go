6c82c288d0d9d7df08a6ec5bb08753df579c8b9a
abassGarane <thelibertinewreck@gmail.com>
2023-01-09 07:23:43 +0300

    use the same jwtsecret as main handler

--- pkg/jwt/create.go
+++ pkg/jwt/create.go
@@ -2,7 +2,7 @@ package jwt
 
 import "github.com/golang-jwt/jwt/v4"
 
-func CreateSignature(claims Claim, secret string) string {
+func CreateSignature(claims *Claim, secret string) string {
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 	tokenStr, _ := token.SignedString([]byte(secret))
 	return tokenStr
